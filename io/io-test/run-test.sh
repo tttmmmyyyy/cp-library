13e1a4085660e46bf90418284026145277737e47
tttmmmyyyy <39012639+tttmmmyyyy@users.noreply.github.com>
2025-07-30 08:33:12 +0900

    update test

--- io/io-test/run-test.sh
+++ io/io-test/run-test.sh
@@ -1,2 +1,6 @@
 fix build
-cat input.txt | valgrind ./a.out
+cat input.txt | ./a.out
+
+# 文字列のscanfの正しさを確認するときに使う
+# ただし、ValgrindはAVX-512命令に対応していないので、AVX-512命令が使われないようなコードに書き換えないと通らない
+# cat input.txt | valgrind ./a.out
